c9b7005f93362455499ee99c2a02662654b8d7e0
zwankier10 <63013984+zwankier10@users.noreply.github.com>
2020-04-23 17:49:20 -0600

    Add files via upload

---
+++ CompMethods_11.m
@@ -0,0 +1,114 @@
+%% Computational Methods Final Project
+
+%%
+clear,clc 
+
+% Intializing Video Objects
+video = 'Zebrafish.mov';
+videoReader = VideoReader(video)
+videoPlayer = vision.VideoPlayer
+
+% Making the video fit the window
+set(0,'showHiddenHandles','on')
+figure = gcf ;  
+figure.findobj 
+fitwindow = figure.findobj ('TooltipString', 'Maintain fit to window');  
+fitwindow.ClickedCallback()  
+
+% Playing the video
+k = 0;
+
+% Get Duration of video and multiply it by 100 to get into ms
+time = videoReader.Duration;
+duration = time*100; %13.85*100=1385 ms
+average_current = zeros(1,duration);
+average_previous = zeros(1,duration);
+time_vector = [0:1:duration-1];
+
+Diff = zeros(duration);
+average_current = zeros(1,duration);
+average_previous = zeros(1,duration);
+while hasFrame(videoReader)
+    % Saves previous frame as the previous data
+    if k > 0
+        subset_previous = frame(region(1):region(1)+region(3),region(2):region(2)+region(4),:);
+        average_previous(1,k) = mean(double(subset_previous), 'all');
+    end
+    
+   frame = readFrame(videoReader);
+   step(videoPlayer,frame);
+   
+% Code to select region of interest on first frame of video
+   if k == 0
+        region = round(getPosition(imrect));
+   end
+   % Inserts the rectangle shape on each frame of the video
+   insertShape(frame, 'rectangle', region);
+   
+   subset_current = frame(region(1):region(1)+region(3),region(2):region(2)+region(4),:);
+   k = k+1;
+   average_current(1,k) = mean(double(subset_current), 'all');
+   
+   % Getting the diff values for each frame
+   placeholder = diff(subset_current);
+   velocity(k) = mean(double(placeholder), 'all')./time;
+
+end
+% Gets rid of the movie player
+delete(figure)
+
+% Finds time between peaks
+peaks = findpeaks(average_current);
+distance = diff(peaks);
+time_between = distance./100;
+
+%% TESTING SECTION- Plot and finding peaks
+
+% In this section I have just been messing around with the time_vector and
+% velocity vector that we found from before in an attempt to see if those
+% values can allow us to recognize where there is a heart beat.  I tried
+% using the value of .07 as a baseline for the speed where the heart moves
+% into the ROI and then moves out.  Looking at the plot visually it looks
+% like there are roughly 44-45 peaks but that is up for interpretation
+% which is bad haha.  But if that is true and each peak measures when the
+% heart moves into and then out of the ROI that would be good because
+% watching the video I count 22 beats during the 13 second video which
+% would match up with the 44 peaks...  Unfortunately using findpeaks on the
+% velocity variable gave me 92 peaks that were above the .07 threshold.
+
+test_vector= ones(length(time_vector));
+
+test_vector= test_vector.*.07;
+
+plot(time_vector, velocity, time_vector, test_vector)
+
+peak2= findpeaks(velocity)
+
+test_find= find(peak2 >= .07)
+
+counter= 0
+
+for ii= 1:length(peak2)
+    if peak2(ii)>=.07
+        counter = counter + 1
+    end
+end
+
+disp(counter)
+
+%%
+
+plot(time_between, 'or')
+
+
+% Calculates average velocity and plots on graph
+trend = mean(velocity);
+trend_vector = ones(1, duration)*trend;
+
+% Plots the velocity per frame (I have it commented out so I can work on
+% other stuff for now without having the graph pop up)
+
+%plot(time_vector, velocity,'b', time_vector, trend_vector, '-r')
+
+
+% Use find peaks to get time on plot
